ab06d3f39a824246b1896f0165e78d48fccdc1b8
Emma Antonucci <emmanicole0920@gmail.com>
2023-04-10 10:24:44 -0600

    commit

---
+++ Controllers/BurialSummaryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buffalo_Intex.Controllers
+{
+    public class BurialSummaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
