45bd7c790f769e5f46c82c8dcd2ddd149da04935
Emma Antonucci <emmanicole0920@gmail.com>
2023-04-10 10:58:12 -0600

    commit

--- Models/SupervisedResponse.cs
+++ Models/SupervisedResponse.cs
@@ -1,8 +1,32 @@
-﻿namespace Buffalo_Intex.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Buffalo_Intex.Models
 {
     public class SupervisedResponse
     {
+        [Key]
+        [Required]
+        public int BurialId { get; set; }
 
+        public decimal MaxCraniumLength { get; set; }
+
+        public decimal MaxCraniumBreadth { get; set; }
+
+        public string SciaticNotch { get; set; }
+
+        public string SubpubicAngle { get; set; }
+
+        public string Femur { get; set; }
+
+        public string FemurHeadDiameter { get; set; } 
+        
+        public int FemurLength { get; set; }
+
+        public string Humerus { get; set; }
+
+        public string HumerusHeadDiameter { get; set; }
+
+        public int HumerusLength { get; set; }
 
     }
 }
